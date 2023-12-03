219dc0c2a6a10a192f0a101e96326bb081ba95e9
shubh045 <shubh6441@gmail.com>
2023-12-03 17:51:22 +0530

    feat: add edit function

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,36 +1,128 @@
+// using hooks
+import { useState } from "react";
 
 // css and icons import
 import { RiDeleteBin7Line } from "react-icons/ri";
 import { FaRegEdit } from "react-icons/fa";
 
-const Users = ({ user, handleChange, users, setUsers, allUsers, setAllUsers }) => {
+const Users = ({
+  user,
+  handleChange,
+  users,
+  setUsers,
+  allUsers,
+  setAllUsers,
+}) => {
+  const [edit, setEdit] = useState(false);
+  const [newItem, setNewItem] = useState({id: "", name: "", email: "", role: "" });
 
   const deleteOne = (id) => {
-    const userToDelete = users.filter(user => user.id === id)
-    const updatedUsers = users.filter(user => user.id !== id);
+    const userToDelete = users.filter((user) => user.id === id);
+    const updatedUsers = users.filter((user) => user.id !== id);
     setUsers(updatedUsers);
 
-    if(allUsers){
-      const allUpdatedUsers = allUsers.filter(user => user !== userToDelete[0])
+    if (allUsers) {
+      const allUpdatedUsers = allUsers.filter(
+        (user) => user !== userToDelete[0]
+      );
       setAllUsers(allUpdatedUsers);
     }
-  }
-  
+  };
+
+  const editHandler = (e) => {
+    const { name, value } = e.target;
+    setNewItem((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleEdit = (id) => {
+    users.map((user) => {
+      if (user.id === id) {
+        setEdit(true);
+        setNewItem({
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          role: user.role,
+        });
+      }
+    });
+  };
+
+  const handleUpdateUser = (id, newValue) => {
+    if (!newValue.name || !newValue.email || !newValue.role) return;
+    setUsers((prev) => prev.map((user) => (user.id === id ? newValue : user)));
+    setEdit(false);
+  };
+
   return (
-    <tr className={user.isChecked ? 'active-row': ''}>
+    <tr className={user.isChecked ? "active-row" : ""}>
       <td>
-        <input type="checkbox" name={user.id} checked={user?.isChecked || false} onChange={handleChange} />
+        <input
+          type="checkbox"
+          name={user.id}
+          checked={user?.isChecked || false}
+          onChange={handleChange}
+        />
       </td>
-      <td>{user.name}</td>
-      <td>{user.email}</td>
-      <td>{user.role}</td>
       <td>
-        <button className="edit">
-          <FaRegEdit />
-        </button>
-        <button className="delete" onClick={() => deleteOne(user.id)}>
-          <RiDeleteBin7Line />
-        </button>
+        {!edit ? (
+          user.name
+        ) : (
+          <input
+            type="text"
+            name="name"
+            value={newItem.name}
+            onChange={editHandler}
+            className="edit-input"
+          ></input>
+        )}
+      </td>
+      <td>
+        {!edit ? (
+          user.email
+        ) : (
+          <input
+            type="email"
+            name="email"
+            value={newItem.email}
+            onChange={editHandler}
+            className="edit-input"
+          ></input>
+        )}
+      </td>
+      <td>
+        {!edit ? (
+          user.role
+        ) : (
+          <input
+            type="text"
+            name="role"
+            value={newItem.role}
+            onChange={editHandler}
+            className="edit-input"
+          ></input>
+        )}
+      </td>
+      <td>
+        {!edit ? (
+          <>
+            <button className="edit" onClick={() => handleEdit(user.id)}>
+              <FaRegEdit />
+            </button>
+            <button className="delete" onClick={() => deleteOne(user.id)}>
+              <RiDeleteBin7Line />
+            </button>
+          </>
+        ) : (
+          <>
+            <button
+              className="save"
+              onClick={() => handleUpdateUser(user.id, newItem)}
+            >
+              Save
+            </button>
+          </>
+        )}
       </td>
     </tr>
   );
